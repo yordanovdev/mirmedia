f89827deea7f0c4ffd0e80decbadc383f84c9dac
Alexander Yordanov <alex123yordanov@gmail.com>
2022-08-05 20:20:01 +0300

    added other create inputs

--- pages/posts/[id]/edit.js
+++ pages/posts/[id]/edit.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useQuill } from "react-quilljs";
 import styles from "../../../styles/PostItemEdit.module.css";
+import createStyles from "../../../styles/Create.module.css";
 import http from "../../../services/http/httpService";
 import { useRouter } from "next/router";
 
 const EditPost = ({ data }) => {
+  const { content, description, imageUrl, title } = data;
+
   const { quill, quillRef } = useQuill();
-  const [text, setText] = useState(data.content);
+  const [text, setText] = useState(content);
+  const [titleText, setTitle] = useState(title);
+  const [descriptionText, setDescription] = useState(description);
+  const [imageUrlText, setImageUrl] = useState(imageUrl);
   const router = useRouter();
 
   useEffect(() => {
@@ -20,14 +26,59 @@ const EditPost = ({ data }) => {
 
   const handleSave = () => {
     http
-      .put("api/services/app/Posts/Update", { ...data, content: text })
+      .put("api/services/app/Posts/Update", {
+        ...data,
+        content: text,
+        title: titleText,
+        description: descriptionText,
+        imageUrl: imageUrlText,
+      })
       .then(() => {
         router.back();
       });
   };
 
   return (
-    <div className={styles.main}>
+    <div className={createStyles.create}>
+      <div className={createStyles.segment}>
+        <h3>Title: </h3>
+        <input
+          placeholder="Title"
+          className={createStyles.input}
+          value={titleText}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div className={createStyles.segment}>
+        <h3>Description: </h3>
+        <input
+          placeholder="Description"
+          className={createStyles.input}
+          value={descriptionText}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+
+      <div className={createStyles.segment}>
+        <h3>Image Url: </h3>
+        <input
+          placeholder="Image url"
+          className={createStyles.input}
+          value={imageUrlText}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            width={150}
+            height={150}
+            className={createStyles.preview}
+          />
+        )}
+      </div>
+
       <div style={{ width: "max-width", height: "fit-content" }}>
         <div ref={quillRef} />
       </div>
