3f8162bd57b1f99c87cca4b9743c88b6fc189422
Alexander Yordanov <alex123yordanov@gmail.com>
2022-08-18 20:23:40 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -21,14 +21,18 @@ const RenderHead = () => {
       <meta property="og:title" content={"Mir Media"} />
       <meta
         property="og:description"
-        content={"Occaecat sint culpa reprehenderit sint cupidatat duis."}
+        content={
+          "Occaecat sint culpa reprehenderit sint cupidatat duis. Aliqua reprehenderit ullamco ipsum amet nisi in occaecat mollit."
+        }
       />
       <meta property="og:type" content="article" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={"Mir Media"} />
       <meta
         name="twitter:description"
-        content={"Culpa magna cillum do consequat occaecat."}
+        content={
+          "Culpa magna cillum do consequat occaecat. Occaecat et excepteur officia non consequat eu non culpa deserunt ipsum consequat."
+        }
       />
     </Head>
   );
