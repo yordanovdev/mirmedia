d8cf567475517992333f255ea842bb22da458ccb
Alexander Yordanov <alex123yordanov@gmail.com>
2022-09-01 14:32:33 +0300

    added render head

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import "quill/dist/quill.snow.css";
 import Loading from "../components/Loading/Loading";
 import Head from "next/head";
+import RenderHead from "../components/RenderHead/RenderHead";
 
 function App({ Component, pageProps }) {
   const Layout = Component.Layout ?? MainLayout;
@@ -10,36 +11,9 @@ function App({ Component, pageProps }) {
     <Layout>
       <Component {...pageProps} />
       <RenderHead />
-      {/* <Loading /> */}
     </Layout>
   );
 }
 
 export default App;
 
-const RenderHead = () => {
-  return (
-    <Head>
-      <meta
-        property="og:image"
-        content="https://mirmedia.vercel.app/images/logo.png"
-      />
-      <meta property="og:title" content={"Mir Media"} />
-      <meta
-        property="og:description"
-        content={
-          "Occaecat sint culpa reprehenderit sint cupidatat duis. Aliqua reprehenderit ullamco ipsum amet nisi in occaecat mollit."
-        }
-      />
-      <meta property="og:type" content="article" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={"Mir Media"} />
-      <meta
-        name="twitter:description"
-        content={
-          "Culpa magna cillum do consequat occaecat. Occaecat et excepteur officia non consequat eu non culpa deserunt ipsum consequat."
-        }
-      />
-    </Head>
-  );
-};
