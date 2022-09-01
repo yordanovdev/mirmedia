6af4daf881f7b34a0016582e1bc32da43551aaa4
Alexander Yordanov <alex123yordanov@gmail.com>
2022-09-01 14:34:22 +0300

    make more stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import MainLayout from "../components/Layout/MainLayout";
 import "../styles/globals.css";
-import "quill/dist/quill.snow.css";
-import Loading from "../components/Loading/Loading";
-import Head from "next/head";
 import RenderHead from "../components/RenderHead/RenderHead";
 
 function App({ Component, pageProps }) {
