f0c4de200baea61207869783399289a1f1d521e6
Alexander Yordanov <alex123yordanov@gmail.com>
2022-09-02 13:10:07 +0300

    scroll

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,16 @@ import "../styles/globals.css";
 import "quill/dist/quill.snow.css";
 import Loading from "../components/Loading/Loading";
 import RenderHead from "../components/RenderHead/RenderHead";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
   const Layout = Component.Layout ?? MainLayout;
+
+  useEffect(() => {
+    window.scrollY(0);
+  }, [router.asPath]);
   return (
     <Layout>
       <Component {...pageProps} />
