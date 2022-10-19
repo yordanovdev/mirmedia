5af7b55a67ad2f544033ae2e3e0f38be560739ae
Alexander Yordanov <alex123yordanov@gmail.com>
2022-10-19 20:00:35 +0300

    extended types

--- types.js
+++ types.js
@@ -1,15 +1,31 @@
 const types = [
   {
-    name: "blog",
-    label: "Blog",
-    image:
-      "https://images.pexels.com/photos/518543/pexels-photo-518543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    name: "home",
+    label: "Начало",
   },
   {
-    name: "news",
-    label: "News",
-    image:
-      "https://images.pexels.com/photos/261579/pexels-photo-261579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    name: "events",
+    label: "Събития",
+  },
+  {
+    name: "sports",
+    label: "Спорт",
+  },
+  {
+    name: "politics",
+    label: "Политика",
+  },
+  {
+    name: "schoolStuff",
+    label: "Ученически неволи",
+  },
+  {
+    name: "music",
+    label: "Музика",
+  },
+  {
+    name: "jokes",
+    label: "Вицове",
   },
 ];
 
